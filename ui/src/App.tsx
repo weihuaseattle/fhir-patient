db296e87e4134450851c9aa668b983d9f226cbf7
Wei Hua <wei@all4cure.com>
2025-06-28 10:50:39 -0700

    Placeholder for projects

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,6 +7,9 @@ import { Home } from '@/pages/Home';
 import { Settings } from '@/pages/Settings';
 import { Page1 } from '@/pages/Page1';
 import { Page2 } from '@/pages/Page2';
+import { Page3 } from '@/pages/Page3';
+import { Page4 } from '@/pages/Page4';
+import { Page5 } from '@/pages/Page5';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   SidebarProvider,
@@ -37,6 +40,9 @@ function AppContent() {
                   <Route path="/" element={<Home />} />
                   <Route path="/page1" element={<Page1 />} />
                   <Route path="/page2" element={<Page2 />} />
+                  <Route path="/page3" element={<Page3 />} />
+                  <Route path="/page4" element={<Page4 />} />
+                  <Route path="/page5" element={<Page5 />} />
                   <Route path="/settings" element={<Settings />} />
                 </Routes>
               </main>
--- ui/src/components/appSidebar.tsx
+++ ui/src/components/appSidebar.tsx
@@ -38,18 +38,42 @@ export function AppSidebar() {
                 </SidebarMenuButton>
               </SidebarMenuItem>
               <SidebarMenuItem>
-                <SidebarMenuButton tooltip="Page 1" isActive={isActive('/page1')} asChild>
+                <SidebarMenuButton tooltip="Proj1 FHIR Patient Management" isActive={isActive('/page1')} asChild>
                   <Link to="/page1">
                     <FileText className="w-4 h-4" />
-                    <span>Page 1</span>
+                    <span>Proj1 FHIR Patient Management</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
               <SidebarMenuItem>
-                <SidebarMenuButton tooltip="Page 2" isActive={isActive('/page2')} asChild>
+                <SidebarMenuButton tooltip="Proj2 SMART Patient App on Epic" isActive={isActive('/page2')} asChild>
                   <Link to="/page2">
                     <Layers className="w-4 h-4" />
-                    <span>Page 2</span>
+                    <span>Proj2 SMART Patient App on Epic</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton tooltip="Proj3 SMART Practitioner App on Cerner" isActive={isActive('/page3')} asChild>
+                  <Link to="/page3">
+                    <Layers className="w-4 h-4" />
+                    <span>Proj3 SMART Practitioner App on Cerner</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton tooltip="Proj4 SMART Backend App on Epic" isActive={isActive('/page4')} asChild>
+                  <Link to="/page4">
+                    <Layers className="w-4 h-4" />
+                    <span>Proj4 SMART Backend App on Epic</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton tooltip="Proj5 FHIR Facade on Postgres" isActive={isActive('/page5')} asChild>
+                  <Link to="/page5">
+                    <Layers className="w-4 h-4" />
+                    <span>Proj5 FHIR Facade on Postgres</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
